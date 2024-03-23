b9aa2a7e6e6245e406ed3bbdf33907fc1a4a91bb
CIRG IIUM <158118611+CIRGIIUM@users.noreply.github.com>
2024-03-23 08:41:21 +0800

    app updates

--- MeIC_app.py
+++ MeIC_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 from keras.models import load_model
-import tensorflow_privacy
+# import tensorflow_privacy
 
 app = Flask(__name__)
 
@@ -34,38 +34,50 @@ def home():
 
 @app.route('/', methods=['POST'])
 def predict():
-    imagefile = request.files['imagefile']
-    dataset = request.form['dataset']
-    
-    if dataset not in model_paths:
-        return jsonify({'error': 'Invalid dataset selection'})
-    
-    model_path = model_paths[dataset]
-    class_label = class_labels[dataset]
-    
-    model = load_model(model_path)
-    
+    try:
+        imagefile = request.files['imagefile']
+        dataset = request.form['dataset']
 
-    image_path = "./images/" + imagefile.filename
-    imagefile.save(image_path)
+        if dataset not in model_paths:
+            return jsonify({'error': 'Invalid dataset selection'})
 
-    image = load_img(image_path, target_size=(128, 128))
-    image = img_to_array(image)
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    image = preprocess_input(image)
-    
-   
-    yhat = model.predict(image)
-    
-  
-    predicted_index = int(yhat.argmax(axis=-1))
+        model_path = model_paths[dataset]
+        class_label = class_labels[dataset]
 
+        print("Model Path:", model_path)  # Debug statement to print model path
 
-    predicted_class = class_label[predicted_index]
-    
-    classification = f'{predicted_class}'
+        model = load_model(model_path)
 
-    return jsonify({'prediction': classification})
+        print("Model Loaded Successfully!")  # Debug statement to confirm model loaded successfully
+
+        image_path = "./images/" + imagefile.filename
+        imagefile.save(image_path)
+
+        # Resize image to match model input shape
+        target_size = (128, 128)  # Adjust dimensions as needed
+        image = Image.open(image_path)
+        image = image.resize(target_size)
+        image = img_to_array(image)
+
+        # Expand dimensions to match model input shape
+        image = np.expand_dims(image, axis=0)
+
+        image = preprocess_input(image)
+
+        yhat = model.predict(image)
+
+        predicted_index = int(yhat.argmax(axis=-1))
+        predicted_class = class_label[predicted_index]
+        classification = f'{predicted_class}'
+
+        return jsonify({'prediction': classification})
+    except Exception as e:
+        # Print the exception traceback to help identify the issue
+        import traceback
+        print("Exception occurred:", e)
+        traceback.print_exc()
+
+        return jsonify({'error': 'An error occurred during prediction. Please try again later.'})
 
 @app.route('/about', methods=['GET'])
 def about():
